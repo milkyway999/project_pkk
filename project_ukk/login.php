a04a86c18e534f1238fe0678e7b8df5dcef72235
milkyway999 <110008286+milkyway999@users.noreply.github.com>
2023-01-23 21:41:46 -0800

    Add files via upload

---
+++ project_ukk/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Login</title>
+</head>
+<body>
+    <!-- ini navbar -->
+<nav class="navbar">
+ <div class="navbar-left">
+  <h1>ini navbar left</h1>
+   </div>
+    <div class="navbar-center">
+     <img class="img-navbar" src="img_nav.png">
+     </div>
+    <div class="navbar-right">
+    
+           </div>
+</nav>
+<!-- ini konten -->
+<div class="container">
+ <div class="card">
+  <h1>ini login</h1>
+   </div>
+</div>
+<!-- ini footer -->
+<footer>
+  <h1>ini footer</h1>
+   </footer>
+
+</body>
+</html>
